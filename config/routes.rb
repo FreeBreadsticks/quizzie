a77d6c08b4ef51f621f1d8d9f21372ed6a5a37fd
Erika <erika.hughes@dbspublishing.com>
2016-03-14 16:18:56 +0000

    Added root page with header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Quizzie::Application.routes.draw do
+
+  root 'worlds#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
